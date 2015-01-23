653a84f8e80e7510a223fde7373be36a276c500b
MartinRemi <mrmi11@yahoo.fr>
2015-01-23 17:13:43 +0100

    Quick fix

--- src/display/window.cpp
+++ src/display/window.cpp
@@ -50,7 +50,7 @@ bool Window::loadMedia()
     bool success = true;
     
     //Load splash image
-    gHelloWorld = SDL_LoadBMP( "img/hello_world.bmp" );
+    gHelloWorld = SDL_LoadBMP( "../img/hello_world.bmp" );
     if( gHelloWorld == NULL )
     {
         printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
