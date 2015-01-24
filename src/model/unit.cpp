9af14681e4e9b151140fe6c943f1ecd6feb018b3
MartinRemi <mrmi11@yahoo.fr>
2015-01-24 16:48:26 +0100

    added constructors for the unit class

--- src/model/unit.cpp
+++ src/model/unit.cpp
@@ -10,6 +10,70 @@
 /**
  *  Comment HARD things
  */
-Unit::Unit() : maxHP(100), HP(100) {
-    
+Unit::Unit() : strength(1), stamina(1), will(1), intelligence(1), luck(1), dexterity(1), lifeSteal(0), penetration(0), lifeRegen(0) {
+    this->computeStats();
+}
+
+Unit::Unit(unsigned short strength, unsigned short stamina, unsigned short will, unsigned short intelligence, unsigned short luck, unsigned short dexterity) : strength(strength), stamina(stamina), will(will), intelligence(intelligence), luck(luck), dexterity(dexterity), lifeSteal(0), penetration(0), lifeRegen(0) {
+    this->computeStats();
+}
+
+void Unit::computeStats() {
+    this->maxHP = this->computeHP();
+    this->HP = this->maxHP;
+    this->maxMana = this->computeMana();
+    this->mana = this->maxMana;
+    this->physicalPower = this->computePhysicalPower();
+    this->magicalPower = this->computeMagicalPower();
+    this->attackSpeed = this->computeAttackSpeed();
+    this->movementSpeed = this->computeMovementSpeed();
+    this->armor = this->computeArmor();
+    this->precision = this->computePrecision();
+    this->manaRegen = this->computeManaRegen();
+    this->dodge = this->computeDodge();
+    this->criticalChance = this->computeCriticalChance();
+}
+
+int Unit::computeHP() {
+    return 100 + this->stamina * 10;
+}
+
+int Unit::computeMana() {
+    return 100 + this->intelligence * 10;
+}
+
+int Unit::computePhysicalPower() {
+    return this->strength * 2 + 10;
+}
+
+int Unit::computeMagicalPower() {
+    return this->intelligence * 2 + 10;
+}
+
+float Unit::computeAttackSpeed() {
+    return this->dexterity / 5;
+}
+
+float Unit::computeMovementSpeed() {
+    return 6 / this->dexterity;
+}
+
+int Unit::computeArmor() {
+    return this->strength * 3;
+}
+
+int Unit::computePrecision() {
+    return (10 + (this->dexterity + 0.5 * this->luck)) * 4;
+}
+
+int Unit::computeManaRegen() {
+    return this->will;
+}
+
+int Unit::computeDodge() {
+    return 2 * this->dexterity;
+}
+
+int Unit::computeCriticalChance() {
+    return this->luck;
 }
--- src/model/unit.h
+++ src/model/unit.h
@@ -24,6 +24,13 @@ class Unit {
     // ----- Function(s)
     protected :
     // ----- Member(s)
+    unsigned short strength;
+    unsigned short stamina;
+    unsigned short dexterity;
+    unsigned short intelligence;
+    unsigned short will;
+    unsigned short luck;
+    
     int maxHP;
     int HP;
     int maxMana;
@@ -36,8 +43,81 @@ class Unit {
     int precision;
     int manaRegen;
     int dodge;
-    int criticalLuck;
+    int criticalChance;
+    int lifeSteal;
+    int penetration;
+    int lifeRegen;
     // ----- Function(s)
+    /**
+     *  Computes all stats
+     */
+    void computeStats();
+    
+    /**
+     *  Computes HP of the unit
+     *  @return the amount of health points of the unit
+     */
+    int computeHP();
+    
+    /**
+     *  Computes mana of the unit
+     *  @return the amount of mana points of the unit
+     */
+    int computeMana();
+    
+    /**
+     *  Computes the physical power of the unit
+     *  @return the amount of physical power of the unit
+     */
+    int computePhysicalPower();
+    
+    /**
+     *  Computes the magical power of the unit
+     *  @return the amount of magical power of the unit
+     */
+    int computeMagicalPower();
+    
+    /**
+     *  Computes the attack speed of the unit
+     *  @return the amount of attack speed of the unit
+     */
+    float computeAttackSpeed();
+    
+    /**
+     *  Computes the movement speed of the unit
+     *  @return the amount of movement speed of the unit
+     */
+    float computeMovementSpeed();
+    
+    /**
+     *  Computes the armore of the unit
+     *  @return the amount of armor of the unit
+     */
+    int computeArmor();
+    
+    /**
+     *  Computes the precision of the unit
+     *  @return the amount of precision of the unit
+     */
+    int computePrecision();
+    
+    /**
+     *  Computes the mana regen of the unit
+     *  @return the amount of mana regen of the unit
+     */
+    int computeManaRegen();
+    
+    /**
+     *  Computes the dodge of the unit
+     *  @return the amount of dodge of the unit
+     */
+    int computeDodge();
+    
+    /**
+     *  Computes the critical chance of the unit
+     *  @return the amount of critical chance of the unit
+     */
+    int computeCriticalChance();
     public :
     // ----- Member(s)
     // ----- Function(s)
@@ -45,6 +125,17 @@ class Unit {
      *  Default constructor with no parameters
      */
     Unit();
+    
+    /**
+     *  Constructor with one parameter for each caracteristic
+     *  @param strength
+     *  @param stamina
+     *  @param will
+     *  @param intelligence
+     *  @param luck
+     *  @param dexterity
+     */
+    Unit(unsigned short strength, unsigned short stamina, unsigned short will, unsigned short intelligence, unsigned short luck, unsigned short dexterity);
 };
 
 #endif /* defined(example__h) */
