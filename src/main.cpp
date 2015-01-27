b03fd913f13d173e5d92abc2a5090047b199cfcd
MartinRemi <mrmi11@yahoo.fr>
2015-01-27 11:14:08 +0100

    Added game loop and quit event

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,33 @@
 
 int main( int argc, char* args[] )
 {
-    Window w;
-    if( !w.init() )
-    {
-        printf( "Failed to initialize!\n" );
-    }
-    else
-    {
-        w.displayImage();
-    }
-    
-    //Free resources and close SDL
-    w.close();
-    
-    return 0;
+	Window w;
+	if( !w.init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		w.displayImage();
+
+		bool quit = false;
+		SDL_Event e;
+
+		// Game loop
+		while( !quit )
+		{
+			//Handle events on queue
+			while( SDL_PollEvent( &e ) != 0 )
+			{
+				//User requests quit
+				if( e.type == SDL_QUIT )
+				{
+					quit = true;
+					w.close();
+				}
+			}
+		}
+	}
+
+return 0;
 }
