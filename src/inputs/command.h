27c106cff76cbcfb14da1de069b9dac5512eaf3d
MartinRemi <mrmi11@yahoo.fr>
2015-02-03 21:08:27 +0100

    Fixed file structe

--- src/inputs/command.h
+++ src/inputs/command.h
@@ -1,5 +1,20 @@
+//
+//  command.cpp
+//  V1 - 03/02/2015
+//
+//  Copyright (c) 2015 Remi&Alex. All rights reserved.
+
+#ifndef command_h
+#define command_h
+
 class Command {
 	public:
+		// ----- Member(s)
+		// ----- Function(s)
 		virtual ~Command() {}
 		virtual void execute() = 0;
+	protected:
+	private:
 };
+
+#endif
--- src/inputs/inputHandler.h
+++ src/inputs/inputHandler.h
@@ -1,14 +1,30 @@
-class InputHandler {
-public:
-	void handleInput();
+//
+//  inputHandler.h
+//  V1 - 03/02/2015
+//
+//  Copyright (c) 2015 Remi&Alex. All rights reserved.
 
-private:
-	Command* button1;
-	Command* button2;
-	Command* button3;
-	Command* button4;
-	Command* upArrow;
-	Command* downArrow;
-	Command* leftArrow;
-	Command* rightArrow;
+#ifndef input_handler_h
+#define input_handler_h
+#include "command.h"
+
+class InputHandler {
+	public:
+		// ----- Member(s)
+		// ----- Function(s)
+		void handleInput();
+	protected:
+	private:
+		// ----- Member(s)
+		Command* button1;
+		Command* button2;
+		Command* button3;
+		Command* button4;
+		Command* upArrow;
+		Command* downArrow;
+		Command* leftArrow;
+		Command* rightArrow;
+		// ----- Function(s)
 };
+
+#endif
