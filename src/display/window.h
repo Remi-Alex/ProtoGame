7acaf4da1d3f3f35562a9900b5c37a29594c6ea6
alsarazin <alsarazin@users.noreply.github.com>
2015-02-06 14:44:25 +0100

    Removed warnings

--- src/display/window.h
+++ src/display/window.h
@@ -12,8 +12,8 @@
 class Window {
     private :
         //Screen dimension constants
-        const int SCREEN_WIDTH = 640;
-        const int SCREEN_HEIGHT = 480;
+        static const int SCREEN_WIDTH = 640;
+        static const int SCREEN_HEIGHT = 480;
     
         //The window we'll be rendering to
         SDL_Window* gWindow;
